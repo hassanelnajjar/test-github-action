767eddaaeab0882a7ae6fddbf8c5722605cc9df0
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-08-09 11:00:39 +0300

    add done to supertest

--- test.js
+++ test.js
@@ -2,18 +2,24 @@ const app = require("./app");
 
 const request = require("supertest");
 
-it("Route / should return status 200 and message : Server running", async () => {
-  const {
-    body: { message },
-  } = await request(app).get("/").expect(200);
-  return expect(message).toBe("Server running");
+it("Route / should return status 200 and message : Server running", (done) => {
+  request(app)
+    .get("/")
+    .expect(200)
+    .then((res) => {
+      expect(res.body.message).toBe("Server running");
+      done();
+    });
 });
 
-it("Route /g10 should return status 200 and message : Hello G10", async () => {
-  const {
-    body: { message },
-  } = await request(app).get("/g10").expect(200);
-  return expect(message).toBe("Hello G10");
+it("Route /g10 should return status 200 and message : Hello G10", (done) => {
+  request(app)
+    .get("/g10")
+    .expect(200)
+    .then((res) => {
+      expect(res.body.message).toBe("Hello G10");
+      done();
+    });
 });
 
 it("sum function will return 3 if first argument =1 and second = 2", () => {
